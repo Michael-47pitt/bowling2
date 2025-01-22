c63a060a1f75b04ba765a786659be90d1d443f9f
nb1190322 <nbeckman314@gmail.com>
2025-01-22 16:47:23 -0500

    Consol branch update

--- BowlingTeamApp.java
+++ BowlingTeamApp.java
@@ -7,16 +7,44 @@ public class BowlingTeamApp
 		
 		Scanner input = new Scanner(System.in);
 		
-		int loopStatus = 5;
+		int loop = true;		//loop control
+		int action = 0;
 		
-		while(loopStatus==5)
+		//main loop
+		while(loopStatus)
 		{
-			System.out.println("Enter a number based
+			//interface print
+			System.out.print("1. Add player\n" + "2. Remove player\n" + 
+			"3. View all players and scores\n" + "4. Update a player's score\n" + "5. Exit app\n");
+			
+			//prompt action number
+			System.out.println("Enter an action number: ");
+			action = input.nextInt();
+			
+			//decide action
+			switch (action) {
+				case 1: 	System.out.println("Enter player name to add: ");
+							String name = input.nextLine();
+							System.out.println("Enter player score: ");
+							int score = input.nextInt();
+							addPlayer(name, score);
+				case 2: 	System.out.println("Enter player name to remove: ");
+							String name = input.nextLine();
+							removePlayer(name);
+				case 3: 	view();
+				case 4: 	System.out.println("Enter name of player to update: ");
+							String name = input.nextLine();
+							System.out.println("Enter updated score: ");
+							int newScore = input.nextInt();
+							updateScore(Team.getPlayer(name), newScore);
+				case 5:		loopStatus = false
+				default: 	System.out.println("ERROR: Invalid input. Please try again.");
+				
 		}
 		
 	}
 	
-	public void addPlayer(String name, int score)
+	public static void addPlayer(String name, int score)
 	{
 		//create player and add to the team
 		Player temp = createNewPlayer(name);
@@ -26,20 +54,20 @@ public class BowlingTeamApp
 		System.out.println(name + " was added to the team");
 	}
 	
-	public void removePlayer(String name)
+	public static void removePlayer(String name)
 	{
 		//remove player from the team
 		removePlayer(name);
 		System.out.println(name + " was removed from the team");
 	}
 	
-	public boolean view()
+	public static void view()
 	{
-		return true;
+		displayPlayers()
 	}
 	
-	public boolean updateScore()
+	public static void updateScore(Player p, int score)
 	{
-		return true;
+		p.updateScore(score);
 	}
 }
