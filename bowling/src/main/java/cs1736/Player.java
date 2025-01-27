da5cd32984380ea94b1dfe76eb5ecdf238a77e1d
ims30 <ims30@pitt.edu>
2025-01-27 12:50:08 -0500

    updated Create player method to a constructor

--- bowling/src/main/java/cs1736/Player.java
+++ bowling/src/main/java/cs1736/Player.java
@@ -10,9 +10,9 @@ public class Player {
     Gson gson = new Gson();
 
     //Methods
-    public boolean createNewPlayer(String name) {
+    public Player(String name) {
         playerName = name;
-        return true;
+        
     }
 
     public int updateScore(int score) {
